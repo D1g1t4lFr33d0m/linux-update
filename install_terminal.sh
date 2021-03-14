c1b2d7f26cea2d6faf6a86d60bfdd2aab14da379
53jk1 <bonczalke@gmail.com>
2021-03-14 12:03:46 +0100

    Two initial shell scripts

---
+++ install_terminal.sh
@@ -0,0 +1,5 @@
+sudo apt install -y git-core zsh curl
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+(cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting)
+(cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions)
+chsh -s $(which zsh)
