c052a1bdf11701784cb761b6fd690ec2aaff64ca
53jk1 <bonczalke@gmail.com>
2021-03-19 22:30:25 +0100

    1.0

---
+++ cmd/add_profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+{
+
+add_list_id=fb358fc9-49ea-4252-ad34-1d25c649e633
+old_list=$(dconf read /org/gnome/terminal/legacy/profiles:/list | tr -d "]")
+
+if [ -z "$old_list" ]
+then
+	front_list="["
+else
+	front_list="$old_list, "
+fi
+
+new_list="$front_list'$add_list_id']"
+dconf write /org/gnome/terminal/legacy/profiles:/list "$new_list" 
+dconf write /org/gnome/terminal/legacy/profiles:/default "'$add_list_id'"
+
+}
