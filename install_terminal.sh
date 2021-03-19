b82289e66476784dbf4cfc9628fc0b43f0577a0e
Kacper Bąk <56700396+53jk1@users.noreply.github.com>
2021-03-19 22:21:47 +0100

    Update install_terminal.sh

--- install_terminal.sh
+++ install_terminal.sh
@@ -1,5 +1,18 @@
+# Install ZSH
 sudo apt install -y git-core zsh curl
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt install -y fonts-powerline
 (cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-syntax-highlighting)
 (cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/zsh-users/zsh-autosuggestions)
+sudo cp configs/.zshrc ~/.zshrc
+sudo cp configs/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 chsh -s $(which zsh)
+
+# Install Patched Font
+mkdir ~/.fonts
+sudo cp -a fonts/. ~/.fonts/
+fc-cache -vf ~/.fonts/
+
+# Color Theme
+dconf load /org/gnome/terminal/legacy/profiles:/:fb358fc9-49ea-4252-ad34-1d25c649e633/ < configs/terminal_profile.dconf
+bash cmd/add_profile.sh
